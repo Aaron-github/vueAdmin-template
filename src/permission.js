135b6905d9452224c980f79baecb8b97e97f3f21
Aaron-github <jahol@vip.qq.com>
2018-05-21 14:24:02 +0800

    部分代码修改

--- src/permission.js
+++ src/permission.js
@@ -1,16 +1,22 @@
 import router from './router'
 import store from './store'
 import NProgress from 'nprogress' // Progress 进度条
-import 'nprogress/nprogress.css'// Progress 进度条样式
-import { Message } from 'element-ui'
-import { getToken } from '@/utils/auth' // 验权
+import 'nprogress/nprogress.css' // Progress 进度条样式
+import {
+  Message
+} from 'element-ui'
+import {
+  getToken
+} from '@/utils/auth' // 验权
 
 const whiteList = ['/login'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
   NProgress.start()
   if (getToken()) {
     if (to.path === '/login') {
-      next({ path: '/' })
+      next({
+        path: '/'
+      })
       NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
       if (store.getters.roles.length === 0) {
@@ -18,8 +24,10 @@ router.beforeEach((to, from, next) => {
           next()
         }).catch((err) => {
           store.dispatch('FedLogOut').then(() => {
-            Message.error(err || 'Verification failed, please login again')
-            next({ path: '/' })
+            Message.error(err || '验证失败, 请重新登录')
+            next({
+              path: '/'
+            })
           })
         })
       } else {
